9a3ad8d98bff2b6d4a88b83b8a4d858f5691413d
Shailit Sharma <shailitsharma@gmail.com>
2024-05-25 01:30:44 +0530

    3-auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -3,25 +3,60 @@ import { Link } from "react-router-dom";
 
 import XSvg from "../../../components/svgs/X";
 
-import { MdOutlineMail } from "react-icons/md";
+import { MdOutlineFamilyRestroom, MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast"
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const queryClient = useQueryClient()
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: async ({ username, password }) => {
+      try {
+        const res = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+
+    onSuccess: () => {
+      toast.success("Login Successful");
+      // refetch the authUser
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    mutate(formData)
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const isError = false;
+
 
   return (
     <div className='max-w-screen-xl mx-auto flex h-screen'>
@@ -47,16 +82,18 @@ const LoginPage = () => {
           <label className='input input-bordered rounded flex items-center gap-2'>
             <MdPassword />
             <input
-              type='current-password'
+              type='password'
               className='grow'
-              placeholder='Password'
+              placeholder='password'
               name='password'
               onChange={handleInputChange}
               value={formData.password}
             />
           </label>
-          <button className='btn rounded-full btn-primary text-white'>Login</button>
-          {isError && <p className='text-red-500'>Something went wrong</p>}
+          <button className='btn rounded-full btn-primary text-white'>
+            {isPending ? "Loading..." : "Login"}
+          </button>
+          {isError && <p className='text-red-500'>{error.message}</p>}
         </form>
         <div className='flex flex-col gap-2 mt-4'>
           <p className='text-white text-lg'>{"Don't"} have an account?</p>
